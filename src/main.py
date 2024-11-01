28e7c17a26059e0f408b4fab7c79fe125455dfe2
Simran509 <75723834+Simran509@users.noreply.github.com>
2024-11-01 22:52:01 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -55,7 +55,7 @@ def createResumeColumnEnum(columns, resumeColumn):
 
 """ 
   Make File name of each resume.pdf
-  Current Format: NAME_BRANCH_ROLL_NUMBER.pdf
+  Current Format: NAME_ROLL_NUMBER.pdf
 """
 def getFileName(row, nameListEnum, rollListEnum):
   fileNames = []
