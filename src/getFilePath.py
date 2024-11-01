82ce9143b42d6c373cb3492367e2d8ad366e5638
Simran509 <75723834+Simran509@users.noreply.github.com>
2024-11-01 21:59:46 +0530

    Update getFilePath.py

--- src/getFilePath.py
+++ src/getFilePath.py
@@ -26,16 +26,6 @@ class App(tk.Tk):
         self.mainloop()
         
 
-    # def getFilePath(self):
-    #     tk.Tk.__init__(self) # create window
-    #     self.title("Resume Extractor")
-    #     self.geometry("500x250")
-    #     label = tk.Label(self, text="Click the Button to browse the File", font=('Georgia 13'))
-    #     label.pack(pady=10)
-    #     self.filename = "" # variable to store filename
-    #     tk.Button(self, text='Browse', command=self.openfile).pack()
-    #     self.mainloop()
-
     def openfile(self):
         self.filename =filedialog.askopenfile(title="open file", mode='r', filetypes=[('csv files', '*.csv'),])
         self.destroy()
@@ -50,24 +40,9 @@ class App(tk.Tk):
 
 
     def getDirectoryPath(self):
-        # tk.Tk.__init__(self) # create window
-        # self.title("Resume Extractor")
-        # self.geometry("500x250")
-        # # self.grid_rowconfigure(0, weight = 1)
-        # # self.grid_columnconfigure(0, weight = 1)
-        # dialog_btn = tk.Button(self, text='Select the Zip File Output Directory', command = self.getDirectory)
-        # dialog_btn.pack(pady=10)
-        # self.mainloop()
         return(self.directoryName)
 
     def getFilePath(self):
-        # self.title("Resume Extractor")
-        # self.geometry("500x250")
-        # label = tk.Label(self, text="Click the Button to browse the CSV File", font=('Georgia 13'))
-        # label.pack(pady=10)
-        # self.filename = "" # variable to store filename
-        # tk.Button(self, text='Browse', command=self.openfile).pack()
-        # self.mainloop()
         return(self.filename.name)
   
 # #  FOR TESTING
